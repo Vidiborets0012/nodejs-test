e9a55f98d3d6f93dd5e3eab8b5fa6c87ef626cfd
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-01-23 19:32:06 +0200

    add dotenv, update error handling middleware

--- src/server.js
+++ src/server.js
@@ -2,9 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import helmet from 'helmet';
+import 'dotenv/config';
 
 const app = express();
-const PORT = 3000;
+// const PORT = 3000;
+
+const PORT = process.env.PORT ?? 3000;
 
 //Логування запитів
 app.use(
@@ -26,9 +29,9 @@ app.use(
 );
 
 // Дозволяє запити з будь-яких джерел
-app.use(cors()); // 2. Захищаємо заголовки
+app.use(cors()); // 2. Перевіряємо доступ
 //Налаштовує різноманітні HTTP-заголовки для захисту від поширених веб-вразливостей
-app.use(helmet()); // 3. Перевіряємо доступ
+app.use(helmet()); // 3. Захищаємо заголовки
 // Middleware для парсингу JSON
 app.use(express.json({ limit: '10mb' })); // 4. Тільки якщо все ок, парсимо дані
 
@@ -49,9 +52,13 @@ app.use((req, res) => {
 // Middleware для обробки помилок
 app.use((err, req, res, next) => {
   console.error('Error:', err.message);
+
+  const isProd = process.env.NODE_ENV === 'production';
+
   res.status(500).json({
-    message: 'Internal Server Error',
-    error: err.message,
+    message: isProd
+      ? 'Something went wrong. Please try again later.'
+      : err.message,
   });
 });
 
