dd7b8ef8ae5c58dfdef24c91f25ee2c241014323
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-01-23 18:59:37 +0200

    add middleware for handling errors and non-existent routes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
+import express from 'express';
 
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello, World!' });
+});
+
+app.get('/test-error', (req, res) => {
+  // Штучна помилка для прикладу
+  throw new Error('Something went wrong');
+});
+
+// Middleware 404 (після всіх маршрутів)
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+// Middleware для обробки помилок
+app.use((err, req, res, next) => {
+  console.error('Error:', err.message);
+  res.status(500).json({
+    message: 'Internal Server Error',
+    error: err.message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
