6283837e94f706e03eef2e187e66735f22601846
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-01-23 19:18:52 +0200

    add JSON + cors + helmet + pino processing (the current standard for configuring Express servers for real projects)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,36 @@
 import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import helmet from 'helmet';
 
 const app = express();
 const PORT = 3000;
+
+//Логування запитів
+app.use(
+  // 1. Логуємо все
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+// Дозволяє запити з будь-яких джерел
+app.use(cors()); // 2. Захищаємо заголовки
+//Налаштовує різноманітні HTTP-заголовки для захисту від поширених веб-вразливостей
+app.use(helmet()); // 3. Перевіряємо доступ
+// Middleware для парсингу JSON
+app.use(express.json({ limit: '10mb' })); // 4. Тільки якщо все ок, парсимо дані
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello, World!' });
